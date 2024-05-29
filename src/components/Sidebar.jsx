7eb8ef8835ba5f1ccc7c9eb1768c784ca6f49abf
Shreejai Raj <shreejairaj@gmail.com>
2024-05-29 22:33:11 +1000

    usestate toggle

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,15 @@
-export default function Sidebar() {
+export default function Sidebar(props) {
+  const { handleToggleModal } = props
   return (
     <div className="sidebar">
-      <div className="bgOverlay"></div>
+      <div onClick={handleToggleModal} className="bgOverlay"></div>
       <div className="sidebarContents">
         <h2>The Brutal Martian Landscape.</h2>
         <div>
           <p>Description</p>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </div>
-        <button>
+        <button onClick={handleToggleModal}>
         <i className="fa-solid fa-arrow-right"></i>
         </button>
       </div>  
