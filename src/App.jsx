27b296d3b62912a17d1972d7341aa618bfef20de
Shreejai Raj <shreejairaj@gmail.com>
2024-05-30 09:59:30 +1000

    data caching

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,25 @@ function App() {
     async function fetchAPIData(){
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
+
+      /*Caching same day data */
+      const today = (new Date()).toDateString();
+      const localKey = `NASA-${today}`
+      if(localStorage.getItem(localKey)){
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log('Fetched from cache today');
+        return
+      }
+      localStorage.clear()
+
       console.log(url);
       try {
         const res = await fetch(url);
         const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData))
         setData(apiData);
-        console.log('DATA\n', apiData);
+        console.log('Fetched from API today');
       } catch (err) {
         console.log(err.message);
       }
